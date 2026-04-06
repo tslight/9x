be7a668ddb015be77d18ae154d53cd02edb9403f
~Not Toby <0x2b@0xff.art>
2026-04-06 11:12:00 +0100

    move tunables to config.h
    Signed-off-by: ~Not Toby <0x2b@0xff.art>

--- 9x.c
+++ 9x.c
@@ -32,29 +32,10 @@
 #include <sys/sysctl.h>
 #endif
 
-#define COL_ACTIVE     0x55AAAA
-#define COL_INACTIVE   0x9EEEEE
-#define COL_MENU_BG    0xE9FFE9
-#define COL_MENU_BG_S  0x448844
-#define COL_MENU_FG    0x000000
-#define COL_MENU_FG_S  0xE9FFE9
-#define COL_MENU_BD    0x88CC88
-#define COL_SWEEP_BD   0xDD0000
-#define COL_ROOT_BG    0x777777
-#define COL_BAR_BG     0x9EEEEE
-#define COL_BAR_FG     0x000000
+#include "config.h"
 
-#define BORDER      4
-#define CORNER      25
-#define MOD         Mod1Mask
-#define XFTFONT     "monospace:size=9"
-#define TERM        "xterm"
 #define MAXCLIENTS  512
 #define INPUTMAX    512
-#define NDESKS      9
-#define BAR_PAD     2
-#define BAR_REFRESH 60
-#define TIMEFMT     "%H:%M %a %d/%m"
 #define MINSIZE     20
 
 #define LENGTH(x)   (sizeof(x) / sizeof((x)[0]))
---
+++ config.h
@@ -0,0 +1,21 @@
+#define COL_ACTIVE     0x55AAAA
+#define COL_INACTIVE   0x9EEEEE
+#define COL_MENU_BG    0xE9FFE9
+#define COL_MENU_BG_S  0x448844
+#define COL_MENU_FG    0x000000
+#define COL_MENU_FG_S  0xE9FFE9
+#define COL_MENU_BD    0x88CC88
+#define COL_SWEEP_BD   0xDD0000
+#define COL_ROOT_BG    0x777777
+#define COL_BAR_BG     0x9EEEEE
+#define COL_BAR_FG     0x000000
+
+#define BORDER      4
+#define CORNER      25
+#define MOD         Mod1Mask
+#define XFTFONT     "monospace:size=9"
+#define TERM        "xterm"
+#define NDESKS      9
+#define BAR_PAD     2
+#define BAR_REFRESH 60
+#define TIMEFMT     "%H:%M %a %d/%m"
