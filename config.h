471bfde7e7a2cc5cbe83e71261871aa6a073b1ae
~Not Toby <0x2b@0xff.art>
2026-04-21 08:38:53 +0100

    "cleanup"
    Signed-off-by: ~Not Toby <0x2b@0xff.art>

--- config.h
+++ config.h
@@ -1,10 +1,7 @@
-/* Colors - Plan 9 palette */
 #define COL_ACTIVE     0x55AAAA    /* active border - teal */
 #define COL_INACTIVE   0x9EEEEE    /* inactive border - pale cyan */
 #define COL_SWEEP_BD   0xAA0000    /* sweep outline - red */
 #define COL_ROOT_BG    0x999999    /* root background - grey */
-
-/* Bar colors */
 #define COL_BAR_BG     0xEAFFFF    /* background - pale cyan */
 #define COL_BAR_FG     0x000000    /* text - black */
 #define COL_BAR_SEL    0x55AAAA    /* selection - teal */
@@ -16,22 +13,16 @@
 #define COL_BAR_DESK1  0xFFEEFF    /* desk 1 window */
 #define COL_BAR_DESK2  0xFFAAFF    /* desk 2 windows */
 #define COL_BAR_DESK3  0xFF77FF    /* desk 3+ windows */
-
-/* Geometry */
-#define BORDER      4
-#define CORNER      32
-
-/* Bar */
-#define BAR_PAD     2
-#define BAR_BTN_PAD 2
-#define BAR_GAP     2
-#define BAR_REFRESH 60
+#define BORDER      	4
+#define CORNER      	32
+#define BAR_PAD     	2
+#define BAR_BTN_PAD 	2
+#define BAR_GAP     	2
+#define BAR_REFRESH 	60
 #define LAUNCH_FILTER_W 200
-
-/* Behavior */
-#define NDESKS      9
-#define DBLCLICK_MS 500
-#define EDGE_SNAP   16
-#define TIMEFMT     "%H:%M %a %d/%m"
-#define TERM        "xterm"
-#define XFTFONT     "sans:bold:size=8"
+#define NDESKS      	9
+#define DBLCLICK_MS 	500
+#define EDGE_SNAP   	16
+#define TIMEFMT     	"%H:%M %a %d/%m"
+#define TERM        	"xterm"
+#define XFTFONT     	"sans:bold:size=8"
